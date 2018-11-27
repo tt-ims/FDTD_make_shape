3cf52c9bac5886698ebf569f2d1d79d2c02c8481
tt-ims <takeuchi@ims.ac.jp>
2018-11-27 12:48:18 +0900

    add periodic boundary condition

--- make_shape.py
+++ make_shape.py
@@ -22,7 +22,8 @@ def write_list(var1,var2):
 ###############################################################################
 #set maximum and initialize variables
 n_s_max=200+1 #+1 is introduced to ensure consistency with the input.
-al=0, 0, 0; dl=0, 0, 0; n_s=0; output='cube'; rot_type='radian';
+al=0, 0, 0; dl=0, 0, 0; n_s=0; iperiodic=0;
+output='cube'; rot_type='radian';
 typ_s=init_1d_list(n_s_max)
 id_s=init_1d_list(n_s_max)
 inf_s=np.zeros((n_s_max,10+1)) #+1 is introduced to ensure consistency with the input.
@@ -36,6 +37,7 @@ f.close()
 tmp_inp = [s.replace('d', 'e') for s in tmp_inp]
 tmp_inp = [s.replace('al_em','al') for s in tmp_inp]
 tmp_inp = [s.replace('el_em','dl') for s in tmp_inp]
+tmp_inp = [s.replace('iperioeic','iperiodic') for s in tmp_inp]
 tmp_inp = [s.replace('ie_s','id_s') for s in tmp_inp]
 tmp_inp = [s.replace('eegree','degree') for s in tmp_inp]
 tmp_inp = [s.replace('raeian','radian') for s in tmp_inp]
@@ -65,10 +67,13 @@ del i
 coo=np.zeros((3,max(g_num)))
 for i in range(3):
     for j in range(g_num[i]):
-        if g_type[i]=='odd':
-            coo[i,j]=(j-(g_num[i]-1)/2)*dl[i]
-        elif g_type[i]=='even':
-            coo[i,j]=(j-g_num[i]/2+0.5)*dl[i]
+        if iperiodic==0:
+            if g_type[i]=='odd':
+                coo[i,j]=(j-(g_num[i]-1)/2)*dl[i]
+            elif g_type[i]=='even':
+                coo[i,j]=(j-g_num[i]/2+0.5)*dl[i]
+        elif iperiodic==3:
+            coo[i,j]=j*dl[i]
 del i, j
 #translate to radian
 if rot_type=='degree':
@@ -78,6 +83,8 @@ if rot_type=='degree':
 ###############################################################################
 shape=np.zeros((g_num[0],g_num[1],g_num[2]))
 cal_tmp=0
+if iperiodic==0: l_max=1
+if iperiodic==3: l_max=27
 for n in range(1,n_s+1): #+1 is introduced to ensure consistency with the input.
     for i in range(g_num[0]):
         for j in range(g_num[1]):
@@ -87,83 +94,101 @@ for n in range(1,n_s+1): #+1 is introduced to ensure consistency with the input.
                 y_tmp=coo[1,j]-ori_s[n,1]
                 z_tmp=coo[2,k]-ori_s[n,2]
                 #rotation
-                x=ma.cos(rot_s[n,2]) \
-                  *( ma.cos(rot_s[n,1])*x_tmp-ma.sin(rot_s[n,1]) \
-                  *(ma.cos(rot_s[n,0])*z_tmp-ma.sin(rot_s[n,0])*y_tmp) ) \
-                  +ma.sin(rot_s[n,2]) \
-                  *(ma.sin(rot_s[n,0])*z_tmp+ma.cos(rot_s[n,0])*y_tmp) 
-                y=-ma.sin(rot_s[n,2]) \
-                  *( ma.cos(rot_s[n,1])*x_tmp-ma.sin(rot_s[n,1]) \
-                  *(ma.cos(rot_s[n,0])*z_tmp-ma.sin(rot_s[n,0])*y_tmp) ) \
-                  +ma.cos(rot_s[n,2]) \
-                  *(ma.sin(rot_s[n,0])*z_tmp+ma.cos(rot_s[n,0])*y_tmp) 
-                z=ma.sin(rot_s[n,1])*x_tmp \
-                  +ma.cos(rot_s[n,1]) \
-                  *(ma.cos(rot_s[n,0])*z_tmp-ma.sin(rot_s[n,0])*y_tmp)
-                #determine shape
-                if typ_s[n]=='ellipsoid':
-                    cal_tmp=(x/(inf_s[n,1]/2))**2 \
-                            + (y/(inf_s[n,2]/2))**2 \
-                            + (z/(inf_s[n,3]/2))**2
-                    if cal_tmp<=1:
-                        shape[i,j,k]=id_s[n]
-                elif typ_s[n]=='half-ellipsoid':
-                    cal_tmp=(x/(inf_s[n,1]/2))**2 \
-                            + (y/(inf_s[n,2]/2))**2 \
-                            + (z/inf_s[n,3])**2
-                    if cal_tmp<=1 and z>=-adj_err:
-                        shape[i,j,k]=id_s[n]
-                elif typ_s[n]=='elliptic-cylinder':
-                    cal_tmp=(x/(inf_s[n,1]/2))**2 \
-                            + (y/(inf_s[n,2]/2))**2
-                    if cal_tmp<=1 and z>=-inf_s[n,3]/2 and z<=inf_s[n,3]/2:
-                        shape[i,j,k]=id_s[n]
-                elif typ_s[n]=='triangular-cylinder':
-                    if x>=-inf_s[n,1]/2 and x<=inf_s[n,1]/2 \
-                    and y>=-inf_s[n,2]/3  \
-                    and y<= inf_s[n,2]/(inf_s[n,1]/2)*x + inf_s[n,2]*2/3 \
-                    and y<=-inf_s[n,2]/(inf_s[n,1]/2)*x + inf_s[n,2]*2/3 \
-                    and z>=-inf_s[n,3]/2 and z<=inf_s[n,3]/2:
-                        shape[i,j,k]=id_s[n]
-                elif typ_s[n]=='rectangular-cylinder':
-                    if x>=-inf_s[n,1]/2 and x<=inf_s[n,1]/2 \
-                    and y>=-inf_s[n,2]/2 and y<=inf_s[n,2]/2 \
-                    and z>=-inf_s[n,3]/2 and z<=inf_s[n,3]/2:
-                        shape[i,j,k]=id_s[n]
-                elif typ_s[n]=='elliptic-cone':
-                    if inf_s[n,3]-z!=0:
-                        cal_tmp=(x/( inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] ))**2 \
-                                + (y/( inf_s[n,2]/2*(inf_s[n,3]-z)/inf_s[n,3] ))**2
-                    else:
-                        cal_tmp=10
-                    if cal_tmp<=1 and z>=-adj_err and z<=inf_s[n,3]:
-                        shape[i,j,k]=id_s[n]
-                elif typ_s[n]=='triangular-cone':
-                    if x>=-inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] \
-                    and x<=inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] \
-                    and y>=-inf_s[n,2]/3*(inf_s[n,3]-z)/inf_s[n,3] \
-                    and y<= inf_s[n,2]/(inf_s[n,1]/2)*x \
-                            +inf_s[n,2]*2/3*(inf_s[n,3]-z)/inf_s[n,3] \
-                    and y<=-inf_s[n,2]/(inf_s[n,1]/2)*x \
-                            +inf_s[n,2]*2/3*(inf_s[n,3]-z)/inf_s[n,3] \
-                    and z>=-adj_err and z<=inf_s[n,3]:
-                        shape[i,j,k]=id_s[n]
-                elif typ_s[n]=='rectangular-cone':
-                    if x>= -inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] \
-                    and x<= inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] \
-                    and y>=-inf_s[n,2]/2*(inf_s[n,3]-z)/inf_s[n,3] \
-                    and y<= inf_s[n,2]/2*(inf_s[n,3]-z)/inf_s[n,3] \
-                    and z>=-adj_err and z<=inf_s[n,3]:
-                        shape[i,j,k]=id_s[n]
-                elif typ_s[n]=='elliptic-ring':
-                    cal_tmp=(x/(inf_s[n,1]/2))**2 \
-                            + (y/(inf_s[n,2]/2))**2
-                    if cal_tmp<=1 and z>=-inf_s[n,3]/2 and z<=inf_s[n,3]/2:
-                        cal_tmp=(x/(inf_s[n,4]/2))**2 \
-                                + (y/(inf_s[n,5]/2))**2
-                        if cal_tmp>=1:
+                x_o=ma.cos(rot_s[n,2]) \
+                    *( ma.cos(rot_s[n,1])*x_tmp-ma.sin(rot_s[n,1]) \
+                    *(ma.cos(rot_s[n,0])*z_tmp-ma.sin(rot_s[n,0])*y_tmp) ) \
+                    +ma.sin(rot_s[n,2]) \
+                    *(ma.sin(rot_s[n,0])*z_tmp+ma.cos(rot_s[n,0])*y_tmp) 
+                y_o=-ma.sin(rot_s[n,2]) \
+                    *( ma.cos(rot_s[n,1])*x_tmp-ma.sin(rot_s[n,1]) \
+                    *(ma.cos(rot_s[n,0])*z_tmp-ma.sin(rot_s[n,0])*y_tmp) ) \
+                    +ma.cos(rot_s[n,2]) \
+                    *(ma.sin(rot_s[n,0])*z_tmp+ma.cos(rot_s[n,0])*y_tmp) 
+                z_o=ma.sin(rot_s[n,1])*x_tmp \
+                    +ma.cos(rot_s[n,1]) \
+                    *(ma.cos(rot_s[n,0])*z_tmp-ma.sin(rot_s[n,0])*y_tmp)
+                #initiate ip
+                ip_x=0; ip_y=0; ip_z=0;
+                for l in range(l_max):
+                    #determine point
+                    x=x_o+al[0]*ip_x
+                    y=y_o+al[1]*ip_y
+                    z=z_o+al[2]*ip_z
+                    #apply periodic boundary condition
+                    if   ip_z== 0: ip_z= 1
+                    elif ip_z== 1: ip_z=-1
+                    elif ip_z==-1: ip_z= 0
+                    if   ip_z== 0 and ip_y== 0: ip_y= 1
+                    elif ip_z== 0 and ip_y== 1: ip_y=-1
+                    elif ip_z== 0 and ip_y==-1: ip_y= 0
+                    if   ip_z== 0 and ip_y== 0 and ip_x== 0: ip_x= 1
+                    elif ip_z== 0 and ip_y== 0 and ip_x== 1: ip_x=-1
+                    elif ip_z== 0 and ip_y== 0 and ip_x==-1: ip_x= 0
+                    #determine shape
+                    if typ_s[n]=='ellipsoid':
+                        cal_tmp=(x/(inf_s[n,1]/2))**2 \
+                                + (y/(inf_s[n,2]/2))**2 \
+                                + (z/(inf_s[n,3]/2))**2
+                        if cal_tmp<=1:
                             shape[i,j,k]=id_s[n]
-del cal_tmp, n, i, j, k, x_tmp, y_tmp, z_tmp, x, y, z
+                    elif typ_s[n]=='half-ellipsoid':
+                        cal_tmp=(x/(inf_s[n,1]/2))**2 \
+                                + (y/(inf_s[n,2]/2))**2 \
+                                + (z/inf_s[n,3])**2
+                        if cal_tmp<=1 and z>=-adj_err:
+                            shape[i,j,k]=id_s[n]
+                    elif typ_s[n]=='elliptic-cylinder':
+                        cal_tmp=(x/(inf_s[n,1]/2))**2 \
+                                + (y/(inf_s[n,2]/2))**2
+                        if cal_tmp<=1 and z>=-inf_s[n,3]/2 and z<=inf_s[n,3]/2:
+                            shape[i,j,k]=id_s[n]
+                    elif typ_s[n]=='triangular-cylinder':
+                        if x>=-inf_s[n,1]/2 and x<=inf_s[n,1]/2 \
+                        and y>=-inf_s[n,2]/3  \
+                        and y<= inf_s[n,2]/(inf_s[n,1]/2)*x + inf_s[n,2]*2/3 \
+                        and y<=-inf_s[n,2]/(inf_s[n,1]/2)*x + inf_s[n,2]*2/3 \
+                        and z>=-inf_s[n,3]/2 and z<=inf_s[n,3]/2:
+                            shape[i,j,k]=id_s[n]
+                    elif typ_s[n]=='rectangular-cylinder':
+                        if x>=-inf_s[n,1]/2 and x<=inf_s[n,1]/2 \
+                        and y>=-inf_s[n,2]/2 and y<=inf_s[n,2]/2 \
+                        and z>=-inf_s[n,3]/2 and z<=inf_s[n,3]/2:
+                            shape[i,j,k]=id_s[n]
+                    elif typ_s[n]=='elliptic-cone':
+                        if inf_s[n,3]-z!=0:
+                            cal_tmp=(x/( inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] ))**2 \
+                                    + (y/( inf_s[n,2]/2*(inf_s[n,3]-z)/inf_s[n,3] ))**2
+                        else:
+                            cal_tmp=10
+                        if cal_tmp<=1 and z>=-adj_err and z<=inf_s[n,3]:
+                            shape[i,j,k]=id_s[n]
+                    elif typ_s[n]=='triangular-cone':
+                        if x>=-inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] \
+                        and x<=inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] \
+                        and y>=-inf_s[n,2]/3*(inf_s[n,3]-z)/inf_s[n,3] \
+                        and y<= inf_s[n,2]/(inf_s[n,1]/2)*x \
+                                +inf_s[n,2]*2/3*(inf_s[n,3]-z)/inf_s[n,3] \
+                        and y<=-inf_s[n,2]/(inf_s[n,1]/2)*x \
+                                +inf_s[n,2]*2/3*(inf_s[n,3]-z)/inf_s[n,3] \
+                        and z>=-adj_err and z<=inf_s[n,3]:
+                            shape[i,j,k]=id_s[n]
+                    elif typ_s[n]=='rectangular-cone':
+                        if x>= -inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] \
+                        and x<= inf_s[n,1]/2*(inf_s[n,3]-z)/inf_s[n,3] \
+                        and y>=-inf_s[n,2]/2*(inf_s[n,3]-z)/inf_s[n,3] \
+                        and y<= inf_s[n,2]/2*(inf_s[n,3]-z)/inf_s[n,3] \
+                        and z>=-adj_err and z<=inf_s[n,3]:
+                            shape[i,j,k]=id_s[n]
+                    elif typ_s[n]=='elliptic-ring':
+                        cal_tmp=(x/(inf_s[n,1]/2))**2 \
+                                + (y/(inf_s[n,2]/2))**2
+                        if cal_tmp<=1 and z>=-inf_s[n,3]/2 and z<=inf_s[n,3]/2:
+                            cal_tmp=(x/(inf_s[n,4]/2))**2 \
+                                    + (y/(inf_s[n,5]/2))**2
+                            if cal_tmp>=1:
+                                shape[i,j,k]=id_s[n]
+del cal_tmp, n, i, j, k, l, l_max, x_tmp, y_tmp, z_tmp, \
+    x_o, y_o, z_o, x, y, z, ip_x, ip_y, ip_z
 ###############################################################################
 #output########################################################################
 ###############################################################################
@@ -243,8 +268,8 @@ elif output == 'mp':
 elapsed_time = ti.time() - start_time
 print ("elapsed time:{0}".format(elapsed_time) + "[sec]")
 #delete all variables
-del al, dl, n_s, output, rot_type, typ_s, id_s, inf_s, ori_s, rot_s, adj_err, \
-    n_s_max, g_num, g_type, coo, shape, \
+del al, dl, n_s, iperiodic, output, rot_type, typ_s, id_s, inf_s, ori_s, rot_s, \
+    adj_err, n_s_max, g_num, g_type, coo, shape, \
     start_time, elapsed_time
 del f, init_1d_list, write_list
 print('Finish!')
